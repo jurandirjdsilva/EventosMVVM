386f2ac42251a2e96175b0a23e5e438309c3e16e
Jurandir Silva <jjr.silva.94@gmail.com>
2018-07-03 17:36:17 -0300

    Class Event: in process...

--- model/Event.py
+++ model/Event.py
@@ -1,13 +1,31 @@
+from .Address import Address
+from datetime import date as dt_date
+from datetime import datetime as dt_datetime
+from datetime import time as dt_time
+
 class Event:
-	def __init__(self, name, address, start_date, final_date, description):
-		self.name = name
-		self.address = address
-		self.start_date = start_date
-		self.final_date = final_date
-		self.description = description
+	def __init__(self, name, address, date, time, description):
+		self.__name = name
+		self.__address = address
+		self.__date = date
+		self.__time = time
+		self.__description = description
 
 	def set_name(self, name):
-		self.name = name
+		self.__name = name
 
 	def set_address(self, address):
-		self.address = address
+		if isinstance(address, Address):
+			self.__address = address
+		else:
+			raise TypeError('the address parameter must be of type Address')
+
+	def set_date(self, date_event):
+		if isinstance(date_event, dt_date):
+			self.__date = date_event
+		else:
+			raise TypeError('the date_event parameter must be of type date')
+
+	def set_time(self, time_event):
+		if isinstance(time_event, dt_time):
+			self.__time = time_event
