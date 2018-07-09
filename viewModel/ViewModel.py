4df22e357a73f5e735f5393fed13b0c7357c2901
ramon jose de sousa araujo <ramon.jsa@gmail.com>
2018-07-09 00:02:55 -0300

    conflito com nomes de pasta

--- viewModel/ViewModel.py
+++ viewModel/ViewModel.py
@@ -1,23 +1,21 @@
 class ViewModel():
     def __init__(self):
         self.viewList = []
-        self.state = "teste estado"
-
+        self.state="teste estado"
+        
     def getState(self):
         return self.state
-
+    
+        
     def setState(self):
-        # busca no modelo
         pass
-
-    def attach(self, view):
-        self.view = view
+    
+    def attach(self,view):
         self.viewList.append(view)
-
-    def deatach(self, view):
+    def deatach(self,view):
         self.viewList.remove(view)
-
+        
     def notify(self):
-        self.view.update()
-        for view in self.viewList:
+        for view in self.viewList :
             view.update(self)
+    
