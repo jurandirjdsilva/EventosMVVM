0b0178f3cb3ce2e22a7400162cf9437e0e0bedd4
Jurandir Silva <jurandirjdsilva@gmail.com>
2018-07-11 13:38:25 -0300

    select_by_period: Ok

--- model/DAO/EventDAO.py
+++ model/DAO/EventDAO.py
@@ -3,8 +3,10 @@ import glob
 import json
 from model.Event import Event
 import os
+from datetime import date as dt_date
 
-def get_all_events():
+
+def select_all_events():
 	files = glob.glob('./model/saved_events/*.json')
 	events = []
 	for f in files:
@@ -16,11 +18,6 @@ def get_all_events():
 			events.append(event)
 	return events
 
-def save_event(event):
-	event_json = json.dumps(event.__dict__)
-	file_name = './model/saved_events/{}.json'.format(event.get_name().upper())
-	with open(file_name, 'w') as f:
-		f.write(event_json)
 
 def select_event(event_name):
 	if event_name in os.listdir('./model/saved_events/'):
@@ -33,6 +30,25 @@ def select_event(event_name):
 		return event
 	else:
 		print('Evento não encontrado!!!')
+
+
+def select_by_period(start=None, end=None):
+	start = start if start is not None else dt_date.min
+	end   = end if end is not None else dt_date.max
+
+	if isinstance(start, dt_date) and isinstance(end, dt_date):
+		events = [e for e in select_all_events() if start <= e <= end]
+	else:
+		raise TypeError("start e end devem ser do tipo date (de datetime)!!!")
+	return events
+
+
+def save_event(event):
+	event_json = json.dumps(event.__dict__)
+	file_name = './model/saved_events/{}.json'.format(event.get_name().upper())
+	with open(file_name, 'w') as f:
+		f.write(event_json)
+
 
 def update_event(event):
 	save_event(event)
